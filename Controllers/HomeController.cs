6d777fb35aac4fb2ed033f7056f5f492a350d573
a.shoeibi <a.shoeibi@alibaba.ir>
2021-04-17 00:02:55 +0430

    Add HttpRequestDto and Index action method.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HttpRequestExample.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HttpRequestExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(string firstName, string lastName)
+        {
+            HttpRequestDto httpRequestDto = new()
+            {
+                QueryStringsCollection = Request.Query,
+                RouteValues = Request.RouteValues,
+                QueryStrings = Request.QueryString,
+                Path = Request.Path,
+                PathBase = Request.PathBase,
+                Host = Request.Host,
+                ContentLength = Request.ContentLength,
+                ContentType = Request.ContentType,
+                IsHttps = Request.IsHttps,
+                Method = Request.Method,
+                Scheme = Request.Scheme,
+                Cookies = Request.Cookies, // Set some cookies to see it.
+                Headers = Request.Headers
+            };
+            return Json(httpRequestDto);
+        }
+    }
+}
---
+++ DTOs/HttpRequestDto.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace HttpRequestExample.DTOs
+{
+    public record HttpRequestDto
+    {
+        public string Scheme { get; init; }
+        public string Method { get; init; }
+        public string ContentType { get; init; }
+        public long? ContentLength { get; init; }
+        public bool IsHttps { get; init; }
+
+        public IRequestCookieCollection Cookies { get; init; }
+        public IHeaderDictionary Headers { get; init; }
+        public IQueryCollection QueryStringsCollection { get; init; }
+
+        public QueryString QueryStrings { get; init; }
+        public PathString Path { get; init; }
+        public PathString PathBase { get; init; }
+        public HostString Host { get; init; }
+
+        public RouteValueDictionary RouteValues { get; init; }
+    }
+}
